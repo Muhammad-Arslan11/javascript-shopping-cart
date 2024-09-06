55cd9079c3ac8b3f0dc2d9770c4763b660ed4eea
Muhammad Arslan Shah <muhammad.arslan9799@gmail.com>
2024-09-06 13:29:41 +0500

    working on increment and decrement function

--- main.js
+++ main.js
@@ -1,51 +1,62 @@
 
- let shop  = document.getElementById("shop");
+let shop = document.getElementById("shop");
 
- // data for shopItems
-  let shopItems = [
-    {
-        image: "./images/img-1.jpg",
-        hieght: "200px",
-        width: "220px",
-        name: "Casual Shirts",
-        details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
-        price: "$ 45"
-    },
 
-    {     image: "./images/img-2.jpg",
-        hieght: "200px",
-        width: "220px",
-        name: "T-shirts",
-        details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
-        price: "$ 60"
-    },
 
-    {       image: "./images/img-3.jpg",
-        hieght: "200px",
-        width: "220px",
-        name: "Office Shirts",
-        details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
-        price: "$ 85"
-    },
+// data for shopItems
+let shopItems = [
+  {
+    id: "0",
+    image: "./images/img-1.jpg",
+    hieght: "200px",
+    width: "220px",
+    name: "Casual Shirts",
+    details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+    price: "$ 45"
+  },
 
-    {   image: "./images/img-4.jpg",
-        hieght: "200px",
-        width: "220px",
-        name: "Mens Suit",
-        details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
-        price: "$ 100"}
-  ]
+  {
+    id: "1",
+    image: "./images/img-2.jpg",
+    hieght: "200px",
+    width: "220px",
+    name: "Office Shirts",
+    details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+    price: "$ 60"
+  },
+
+
+  {
+    id: "2",
+    image: "./images/img-3.jpg",
+    hieght: "200px",
+    width: "220px",
+    name: "T-shirts",
+    details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+    price: "$ 85"
+  },
+
+  {
+    id: "3",
+    image: "./images/img-4.jpg",
+    hieght: "200px",
+    width: "220px",
+    name: "Mens Suit",
+    details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+    price: "$ 100"
+  }
+]
 
 
 
 let quantity = 0; // quantity: for product quantity
-  // make a function to generate cards
-  let generateCard = () =>{
-    
-        // run a map function over the shopItems
-    return (shop.innerHTML =  shopItems.map((item) =>{
-    return(
-        `<div class="item">
+// make a function to generate cards
+let generateCard = () => {
+
+  // run a map function over the shopItems
+  return (shop.innerHTML = shopItems.map((item) => {
+    return (
+      `<div class="item" id=${item.id}>
           <div><img src="${item.image}" width="${item.width}" height="${item.hieght}"></div>
           <div class="desc">
             <h2 class="name">${item.name}</h2>
@@ -54,34 +65,33 @@ let quantity = 0; // quantity: for product quantity
           <div class="price-quantity">
             <p class="price">${item.price}</p>
             <div class="buttons">
-              <i class="bi bi-plus"></i>
+              <i onclick="increment(${item.id})" class="bi bi-plus"id=></i>
               <p class="quantity" id="quantity">${quantity}</p>
-              <i class="bi bi-dash"></i>
+              <i onclick="decrement(${item.id})" class="bi bi-dash"${item.id}></i>
             </div>
           </div>
         </div>
         `
 
     )
-        }) 
-    ); 
-  }
-  generateCard();
+  }).join(" ")
+  );
+}
+generateCard();
 
 // increment quantity
-let plusBtn = document.querySelectorAll("bi bi-plus");
-plusBtn.addEventListener("click", function(){
-    quantity += 1;
-    // update the quantity
- document.querySelector(".quantity").textContent = quantity;
-});
+function increment(id) {
+  // console.log(id)
+quantity += 1;
+  // update the quantity
+}
 
 // decrement quantity
- let minusBtn = document.querySelectorAll("bi bi-dash");
- minusBtn.addEventListener("click", function(){
-    if(quantity != 0){
-        quantity -= 1;
-    }
+function decrement(e) {
+
+  if (quantity > 0) {
+    quantity -= 1;
     // update the quantity
- document.querySelector(".quantity").textContent = quantity;
-});
+
+ }
+}
