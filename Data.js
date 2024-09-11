0718825e7dce5b44c4ecd6956d2ba124c6d92327
Muhammad Arslan Shah <muhammad.arslan9799@gmail.com>
2024-09-11 19:35:37 +0500

    new issue found

--- Data.js
+++ Data.js
@@ -1,48 +1,4 @@
 // data for shopItems
- export let shopItems = [
-  {
-    id: "0",
-    image: "./images/img-1.jpg",
-    hieght: "200px",
-    width: "220px",
-    name: "Casual Shirts",
-    details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
-    price: "$ 45",
-
-  },
-
-  {
-    id: "1",
-    image: "./images/img-2.jpg",
-    hieght: "200px",
-    width: "220px",
-    name: "Office Shirts",
-    details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
-    price: "$ 60",
-   
-  },
 
 
-  {
-    id: "2",
-    image: "./images/img-3.jpg",
-    hieght: "200px",
-    width: "220px",
-    name: "T-shirts",
-    details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
-    price: "$ 85",
-   
-  },
-
-  {
-    id: "3",
-    image: "./images/img-4.jpg",
-    hieght: "200px",
-    width: "220px",
-    name: "Mens Suit",
-    details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
-    price: "$ 100",
-   
-  }
-]
 
--- main.js
+++ main.js
@@ -1,6 +1,54 @@
 
-import {shopItems} from "./Data.js";
 let shop = document.getElementById("shop");
+
+
+let shopItems = [
+  {
+    id: "0",
+    image: "./images/img-1.jpg",
+    hieght: "200px",
+    width: "220px",
+    name: "Casual Shirts",
+    details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+    price: "$ 45",
+
+  },
+
+  {
+    id: "1",
+    image: "./images/img-2.jpg",
+    hieght: "200px",
+    width: "220px",
+    name: "Office Shirts",
+    details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+    price: "$ 60",
+   
+  },
+
+
+  {
+    id: "2",
+    image: "./images/img-3.jpg",
+    hieght: "200px",
+    width: "220px",
+    name: "T-shirts",
+    details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+    price: "$ 85",
+   
+  },
+
+  {
+    id: "3",
+    image: "./images/img-4.jpg",
+    hieght: "200px",
+    width: "220px",
+    name: "Mens Suit",
+    details: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+    price: "$ 100",
+   
+  }
+]
+
 
 
 // create a bucket for Items
@@ -116,5 +164,22 @@ function decrement(id) {
    }
    calculate();
 
+
+    // generate cart images function
+     let generate = ()=>{
+       let shopping_cart = document.getElementById("shopping-cart");
+       let label = document.getElementById("label");
+
+      if(bucket.length !== 0){
+
+      }else{
+        shopping_cart.innerHTML = ``;
+        label.innerHTML = `
+        <h2>Cart is Empty</h2>
+         <a href="index.html"><h1>Back to Home</h1></a>
+        `;
+      }
+     }
+     generate();
    
 
